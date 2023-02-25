ce5b6617ed1903292cf1bf78d7e154fb175d9f8c
Maxim Elushev <68665764+Flockk@users.noreply.github.com>
2023-02-25 21:53:39 +0300

    внесены поправки

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -26,7 +26,7 @@ bool isFull(queue *q) {
 
 // Добавление элемента в конец очереди
 void enqueue(queue *q, int x) {
-    std::cout << "Добавлено " << x << " \n";
+    std::cout << "\nДобавлено число " << x << " \n";
     if (isFull(q)) {
         std::cout << "Ошибка: Очередь заполнена\n";
         return;
@@ -41,26 +41,31 @@ void enqueue(queue *q, int x) {
 
 // Удаление элемента из начала очереди
 void dequeue(queue *q) {
-    std::cout << "Удалено \n";
     if (isEmpty(q)) {
-        std::cout << "Ошибка: Очередь пуста\n";
+        std::cout << "\nОшибка: Очередь пуста\n";
         return;
     } else if (q->front == q->rear) {
+        std::cout << "\nУдалено число " << q->array[q->front] << " \n";
         q->rear = q->front = -1;
     } else {
+        std::cout << "\nУдалено число " << q->array[q->front] << " \n";
         q->front = (q->front + 1) % kMaxSize;
     }
 }
 
 // Вывод текущего состояния очереди
 void printQueue(queue *q) {
-    int count = (q->rear + kMaxSize - q->front) % kMaxSize + 1;
-    std::cout << "Очередь: ";
-    for (int i = 0; i < count; i++) {
-        int index = (q->front + i) % kMaxSize;
-        std::cout << q->array[index] << " ";
+    if (isEmpty(q)) {
+        std::cout << "\nОчередь пуста!\n";
+    } else {
+        int count = (q->rear + kMaxSize - q->front) % kMaxSize + 1;
+        std::cout << "\nОчередь: ";
+        for (int i = 0; i < count; i++) {
+            int index = (q->front + i) % kMaxSize;
+            std::cout << q->array[index] << " ";
+        }
+        std::cout << "\n";
     }
-    std::cout << "\n\n";
 }
 
 // Ввод целочисленного значения с проверкой
@@ -105,54 +110,50 @@ int failure(int begin, int end) {
 void callMenu(queue *q) {
     bool work{true};
     while (work) {
-        std::cout << "...................................................\n";
-        std::cout << "\nВведите номер команды:\n";
-        std::cout << "1. Проверка пустоты очереди\n";
-        std::cout << "2. Проверка заполненности очереди\n";
-        std::cout << "3. Добавить элемент в конец очереди\n";
-        std::cout << "4. Удалить элемент из начала очереди\n";
-        std::cout << "5. Вывод текущего состояния очереди на экран\n";
+        std::cout << "...................................................";
+        std::cout << "\nВарианты команд:\n";
+        std::cout << "1. Вывод текущего состояния очереди на экран\n";
+        std::cout << "2. Добавить элемент в конец очереди\n";
+        std::cout << "3. Удалить элемент из начала очереди\n";
+        std::cout << "4. Проверка пустоты очереди\n";
+        std::cout << "5. Проверка заполненности очереди\n";
         std::cout << "6. Завершить работу программы\n";
+        std::cout << "...................................................\n";
         std::cout << "Введите номер команды: ";
 
         int choice = failure(1, 6);
         switch (choice) {
             case 1:
-                if (isEmpty(q)) {
-                    std::cout << "Очередь пуста";
-                } else {
-                    std::cout << "Очередь не пуста";
-                }
-                std::cout << "\n\n";
+                printQueue(q);
                 break;
             case 2:
                 if (isFull(q)) {
-                    std::cout << "Очередь полна";
-                } else {
-                    std::cout << "Очередь не полна";
-                }
-                std::cout << "\n\n";
-                break;
-            case 3:
-                if (isFull(q)) {
-                    std::cout << "Ошибка: Очередь полна!\n\n";
+                    std::cout << "\nОшибка: Очередь полна!\n";
                     break;
                 }
-                std::cout << "Введите число для добавления в очередь: ";
+                std::cout << "\nВведите число для добавления в очередь: ";
                 enqueue(q, failure());
-                std::cout << "\n\n";
+                break;
+            case 3:
+                dequeue(q);
                 break;
             case 4:
-                dequeue(q);
-                std::cout << "\n\n";
+                if (isEmpty(q)) {
+                    std::cout << "\nОчередь пуста\n";
+                } else {
+                    std::cout << "\nОчередь не пуста\n";
+                }
                 break;
             case 5:
-                printQueue(q);
-                std::cout << "\n\n";
+                if (isFull(q)) {
+                    std::cout << "\nОчередь полна\n";
+                } else {
+                    std::cout << "\nОчередь не полна\n";
+                }
                 break;
             case 6:
                 work = false;
-                std::cout << "\n\n";
+                std::cout << "\nПрограмма завершена...\n";
                 break;
             default:
                 break;
