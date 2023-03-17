caee16997504174843d4191d75cd9b6cee8198f4
Maxim Elushev <maxim.elushev@yandex.ru>
2023-03-17 12:21:20 +0300

    изменение работы удаления элемента

--- Lab4/main.cpp
+++ Lab4/main.cpp
@@ -57,6 +57,25 @@ void insert(ArrayList *my_list, int item) {
     my_list->count++;
 }
 
+// Функция удаления элемента по его значению
+void remove(ArrayList *my_list, int value) {
+    int i = 0;
+    while (i < my_list->count && my_list->array[i] != value) {
+        i++; // находим первый элемент, который равен значению
+    }
+    if (i < my_list->count && my_list->array[i] == value) { // проверяем, существует ли элемент с заданным значением
+        std::cout << "\nУдален элемент со значением " << value << "\n";
+        while (i < my_list->count - 1) {
+            my_list->array[i] = my_list->array[i + 1];
+            i++;
+        }
+        my_list->array[my_list->count - 1] = 0; // удаляем последний элемент
+        my_list->count--; // уменьшаем количество элементов в списке
+    } else {
+        std::cout << "\nЭлемент со значением " << value << " не найден. Удаление невозможно!\n";
+    }
+}
+
 // Функция для поиска элемента с заданной информационной частью
 int search(ArrayList *my_list, int value) {
     for (int i = 0; i < my_list->count; i++) { // проходим по всем элементам списка
@@ -66,19 +85,6 @@ int search(ArrayList *my_list, int value) {
     }
     return -1;  // элемент не найден
 }
-
-// Функция удаления элемента по его индексу
-void remove(ArrayList *my_list, int index) {
-    // Сдвигаем все элементы, начиная с позиции index + 1, на одну позицию влево
-    for (int i = index; i < my_list->count - 1; i++) {
-        my_list->array[i] = my_list->array[i + 1]; // каждый элемент списка, начиная с элемента
-    }
-
-    // Обнуляем последний элемент и уменьшаем количество элементов на 1
-    my_list->array[my_list->count - 1] = 0; // последний элемент удаляем, обнуляя его
-    my_list->count--; // уменьшаем количество элементов в списке на 1
-}
-
 
 // Функция для отображения текущего состояния списка
 void printList(ArrayList *my_list) {
@@ -131,13 +137,14 @@ int failure(int begin, int end) {
 void callMenu(ArrayList *my_list) {
     bool work{true};
     while (work) {
-        std::cout << "...................................................\n";
-        std::cout << "\nВведите номер команды: \n";
+        std::cout << "________________________________________________";
+        std::cout << "\nВарианты команд:\n";
         std::cout << "1. Вывести текущее состояние списка\n";
         std::cout << "2. Добавить элемент в список\n";
-        std::cout << "3. Удаление элемента по индексу\n";
+        std::cout << "3. Удаление элемента по значению\n";
         std::cout << "4. Поиск элемента\n";
         std::cout << "5. Завершить работу программы\n";
+        std::cout << "________________________________________________\n";
         std::cout << "Введите номер команды: ";
 
         int choice = failure(1, 5);
@@ -162,18 +169,18 @@ void callMenu(ArrayList *my_list) {
                 break;
             case 3:
                 if (isEmpty(my_list)) {
-                    std::cout << "\nСписок пуст\n";
+                    std::cout << "\nСписок пуст. Удаление невозможно!\n";
                     break;
                 }
-                int remove_index;
-                std::cout << "\nВведите индекс элемента, который вы хотите удалить: ";
-                remove_index = failure(0, my_list->count - 1);
-                remove(my_list, remove_index);
+                int remove_value;
+                std::cout << "\nВведите значение элемента, который вы хотите удалить: ";
+                remove_value = failure();
+                remove(my_list, remove_value);
                 std::cout << "\n";
                 break;
             case 4:
                 if (isEmpty(my_list)) {
-                    std::cout << "\nСписок пуст\n";
+                    std::cout << "\nСписок пуст. Поиск невозможен!\n";
                     break;
                 }
                 int value_index;
