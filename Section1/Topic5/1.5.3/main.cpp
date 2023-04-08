29c3f5296eedff389124e83ab160b7f778b3f159
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-08 23:07:21 +0300

    added a full add and search function

--- Section1/Topic5/1.5.3/main.cpp
+++ Section1/Topic5/1.5.3/main.cpp
@@ -55,6 +55,19 @@ bool isEmpty() {
     return root == nullptr;
 }
 
+// Функция поиска вершины в дереве
+void search(TreeNode *pNode, int intToFind) {
+    if (shouldContinue && pNode != nullptr) {
+        if (pNode->data == intToFind) {
+            detectedNode = pNode;
+            shouldContinue = false;
+            std::cout << "\nВершина со значением " << intToFind << " найдена в дереве." << std::endl;
+        }
+        search(pNode->left, intToFind);
+        search(pNode->right, intToFind);
+    }
+}
+
 // Функция добавления корня
 void addRoot() {
     root = new TreeNode();
@@ -63,6 +76,64 @@ void addRoot() {
     root->left = nullptr;
     root->right = nullptr;
     std::cout << "\nДобавлена корневая вершина со значением " << root->data << std::endl;
+}
+
+// Функция добавления левого потомка
+void addLeft(TreeNode *pCurr) {
+    std::cout << "У данной вершины нет левого потомка. Введите значение для добавления: ";
+    int intToAdd = failure();
+    auto *pTemp = new TreeNode();
+    pTemp->left = nullptr;
+    pTemp->right = nullptr;
+    pTemp->data = intToAdd;
+    pCurr->left = pTemp;
+    std::cout << "\nДобавлена вершина со значением " << pTemp->data << " в качестве левого потомка.\n";
+}
+
+// Функция добавления правого потомка
+void addRight(TreeNode *pCurr) {
+    std::cout << "У данной вершины нет правого потомка. Введите значение для добавления: ";
+    int intToAdd = failure();
+    auto *pTemp = new TreeNode();
+    pTemp->left = nullptr;
+    pTemp->right = nullptr;
+    pTemp->data = intToAdd;
+    pCurr->right = pTemp;
+    std::cout << "\nДобавлена вершина со значением " << pTemp->data << " в качестве правого потомка.\n";
+}
+
+// Функция добавления новой вершины
+void addNewNode(TreeNode *pCurr, int whereToAdd) {
+    std::cout << "\nВведите значение для новой вершины: ";
+    int intToAdd = failure();
+    auto *pTemp = new TreeNode();
+    pTemp->left = nullptr;
+    pTemp->right = nullptr;
+    pTemp->data = intToAdd;
+    if (whereToAdd == 1) {
+        pCurr->left = pTemp;
+        std::cout << "\nДобавлена вершина со значением " << pTemp->data << " в качестве левого потомка.\n";
+    } else {
+        pCurr->right = pTemp;
+        std::cout << "\nДобавлена вершина со значением " << pTemp->data << " в качестве правого потомка.\n";
+    }
+}
+
+// Функция добавления вершин
+void addNodes(TreeNode *pCurr) {
+    if (pCurr->left != nullptr && pCurr->right != nullptr) {
+        std::cout << "У данной вершины уже есть оба потомка.\n";
+    } else if (pCurr->left == nullptr && pCurr->right != nullptr) {
+        addLeft(pCurr);
+    } else if (pCurr->left != nullptr && pCurr->right == nullptr) {
+        addRight(pCurr);
+    } else {
+        int whereToAdd;
+        std::cout << "У данной вершины нет потомков. В какую сторону добавить новую вершину?";
+        std::cout << "\n1. Влево\n2. Вправо\nВаш выбор: ";
+        whereToAdd = failure(1, 2);
+        addNewNode(pCurr, whereToAdd);
+    }
 }
 
 // Функция вывода дерева в обратно-симметричном порядке
@@ -88,8 +159,9 @@ void callMenu() {
         std::cout << "_______________________________________________________________________";
         std::cout << "\nВведите номер команды: \n";
         std::cout << "1. Вывести текущее состояние дерева\n";
-        std::cout << "2. Добавление вершины\n";
-        std::cout << "3. Завершение работы\n";
+        std::cout << "2. Добавление потомка вершины дерева\n";
+        std::cout << "3. Поиск заданной вершины дерева\n";
+        std::cout << "4. Завершение работы\n";
         std::cout << "_______________________________________________________________________\n";
         std::cout << "Введите номер команды: ";
         choice = failure(1, 5);
@@ -109,8 +181,28 @@ void callMenu() {
                     addRoot();
                     break;
                 }
+                std::cout << "\nВведите информационную часть вершины, которой следует добавить потомка: ";
+                search(root, failure());
+                if (detectedNode == nullptr) {
+                    std::cout << "\nТакой вершины не существует!\n";
+                } else {
+                    addNodes(detectedNode);
+                }
                 break;
-            case 5:
+            case 3:
+                detectedNode = nullptr;
+                shouldContinue = true;
+                if (isEmpty()) {
+                    std::cout << "\nДерево пустое. Поиск невозможен!" << std::endl;
+                    break;
+                }
+                std::cout << "\nВведите значение вершины, которую хотите найти: ";
+                search(root, failure());
+                if (detectedNode == nullptr) {
+                    std::cout << "\nВершина не найдена в дереве." << std::endl;
+                }
+                break;
+            case 4:
                 work = false;
                 std::cout << "\nРабота программы завершена.\n";
                 break;
