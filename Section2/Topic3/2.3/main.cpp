002703be142db24ec64d4f68bd41f43d49e8b99a
Maxim Elushev <maxim.elushev@yandex.ru>
2023-04-10 02:30:07 +0300

    bucket sort

--- Section2/Topic3/2.3/main.cpp
+++ Section2/Topic3/2.3/main.cpp
@@ -5,6 +5,11 @@
 
 const int kMaxSize = 10000;
 
+struct ListItem {
+    int data;
+    ListItem *next;
+};
+
 // Функция создания массива с уникальными ключами
 void createUniqueArray(int *arr, int size) {
     for (int i = 0; i < size; i++) {
@@ -66,6 +71,56 @@ void simpleBucketSortNoBucket(int *data, int size, int &compares, int &swaps) {
     std::cout << "Количество сравнений: " << compares << std::endl;
 }
 
+// Функция добавления элемента в связный список
+void addItem(ListItem *pItem, int value) {
+    auto *pNew = new ListItem;
+    pNew->data = value;
+    pNew->next = nullptr;
+    while (pItem->next != nullptr) {
+        pItem = pItem->next;
+    }
+    pItem->next = pNew;
+}
+
+// Функция очищения списка
+void clearList(ListItem *head) {
+    while (head != nullptr) {
+        ListItem *temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+// Функция отсортированного массива при помощи обобщенной карманной сортировки
+void showBucket(ListItem *data, int n) {
+    std::cout << "\nОтсортированный массив:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        ListItem *current = data[i].next;
+        while (current != nullptr) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+    }
+    std::cout << std::endl;
+}
+
+// Функция выполнения обобщенной карманной сортировки
+void bucketSort(int *data, int size, int &swaps) {
+    swaps = 0;
+    auto *sortedArray = new ListItem[size];
+    for (int i = 0; i < size; i++) {
+        sortedArray[i].next = nullptr;
+    }
+    for (int i = 0; i < size; i++, swaps++) {
+        addItem(&sortedArray[data[i]], data[i]);
+    }
+    showBucket(sortedArray, size);
+    std::cout << "\nКоличество перестановок: " << swaps << std::endl;
+    for (int i = 0; i < size; i++)
+        clearList(sortedArray[i].next);
+    delete[] sortedArray;
+}
+
 // Ввод целочисленного значения с проверкой интервала
 int failure(int begin, int end) {
     int choice;
@@ -98,10 +153,11 @@ void callMenu() {
         std::cout << "\nВведите номер команды: \n";
         std::cout << "1. Простейшая карманная сортировка с использованием второго массива\n";
         std::cout << "2. Простейшая карманная сортировка без использованием второго массива\n";
-        std::cout << "3. Завершение работы\n";
+        std::cout << "3. Обобщённая карманная сортировка\n";
+        std::cout << "4. Завершение работы\n";
         std::cout << "__________________________________________________________________________\n";
         std::cout << "Введите номер команды: ";
-        int choice = failure(1, 3);
+        int choice = failure(1, 4);
         switch (choice) {
             case 1:
                 std::cout << "Введите размер массива (до " << kMaxSize << "): ";
@@ -122,6 +178,17 @@ void callMenu() {
                 simpleBucketSortNoBucket(mainArray, size, compares, swaps);
                 break;
             case 3:
+                std::cout << "Введите размер массива (до " << kMaxSize << "): ";
+                size = failure(1, kMaxSize);
+                mainArray = new int[size];
+                for (int i = 0; i < size; i++) {
+                    mainArray[i] = rand() % size;
+                }
+                std::cout << "\nНеотсортированный массив: \n";
+                printArray(mainArray, size);
+                bucketSort(mainArray, size, swaps);
+                break;
+            case 4:
                 work = false;
                 std::cout << "\nРабота программы завершена.\n";
                 break;
